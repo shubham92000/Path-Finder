437e3053dc49495860e9e01261c455e445662b0d
shubham92000 <sbhsingh616@gmail.com>
2021-05-27 20:51:00 +0530

    removed

--- build/thirdparty/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/shubham/cpp/cpp_projects/Path_Finder/thirdparty/googletest
-# Build directory: /home/shubham/cpp/cpp_projects/Path_Finder/build/thirdparty/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- build/thirdparty/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/shubham/cpp/cpp_projects/Path_Finder/thirdparty/googletest/googlemock
-# Build directory: /home/shubham/cpp/cpp_projects/Path_Finder/build/thirdparty/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
